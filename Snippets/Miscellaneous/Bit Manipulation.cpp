2f80b2f041a9141bb5b459aa53f0e1b4aeff28c2
Hossain Mahmud <hmsayem@gmail.com>
2021-11-20 19:15:59 +0600

    Update Bit Manipulation.cpp

--- Snippets/Miscellaneous/Bit Manipulation.cpp
+++ Snippets/Miscellaneous/Bit Manipulation.cpp
@@ -1,19 +1,17 @@
-int Set(int N, int pos)   {return N = N | (1<<pos);}
+int Set(int N, int pos)   { return N = N | (1<<pos); }
 
-int Reset(int N, int pos) {return N = N & ~(1<<pos);}
+int Reset(int N, int pos) { return N = N & ~(1<<pos); }
 
-bool check(int N,int pos) {return (bool)(N & (1<<pos));}
+bool check(int N,int pos) { return (bool)(N & (1<<pos)); }
 
-int toggle(int n, int k)  {return n ^= 1 << k;}
+int toggle(int n, int k)  { return n ^= 1 << k; }
 
 
-bool isPowerOfTwo(long long x)
-{
+bool isPowerOfTwo(long long x) {
     return (x && !(x & (x - 1)));
 }
 
-long long largest_power( long long N)
-{
+long long largest_power(long long N) {
       N = N| (N >> 1);
       N = N| (N >> 2);
       N = N| (N >> 4);
